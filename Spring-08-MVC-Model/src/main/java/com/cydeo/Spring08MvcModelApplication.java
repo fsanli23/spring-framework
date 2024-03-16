80e4b346790c8870d4283904f0dada9b16378fd5
fsanli23 <sanlifatih23@gmail.com>
2024-03-16 12:26:30 -0500

    Spring Boot MVC controller

---
+++ Spring-08-MVC-Model/src/main/java/com/cydeo/Spring08MvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08MvcModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08MvcModelApplication.class, args);
+    }
+
+}
---
+++ Spring-08-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homePage(Model model) {
+
+        model.addAttribute("name","fatih");
+        model.addAttribute("course","MVC");
+        return "student/welcome";
+    }
+}
