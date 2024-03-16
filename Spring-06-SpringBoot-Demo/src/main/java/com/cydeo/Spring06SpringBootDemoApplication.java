9f0a62bd03deaa4dae58c39cd0eebbc1dbfd36e1
fsanli23 <sanlifatih23@gmail.com>
2024-03-15 22:46:12 -0500

    Spring Boot demo

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/Spring06SpringBootDemoApplication.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication()
+public class Spring06SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("johnson");
+        comment.setText("Spring Framework");
+        ApplicationContext context = SpringApplication.run(Spring06SpringBootDemoApplication.class, args);
+        CommentService cs = context.getBean(CommentService.class);
+
+        cs.publishComment(comment);
+
+
+    }
+
+}
---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,21 @@
+package com.cydeo.proxy;
+
+import com.cydeo.model.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class EmailCommentNotificationProxy implements CommentNotificationProxy {
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending notification  Email for comment : " + comment.getText());
+    }
+
+
+
+
+
+
+
+
+}
