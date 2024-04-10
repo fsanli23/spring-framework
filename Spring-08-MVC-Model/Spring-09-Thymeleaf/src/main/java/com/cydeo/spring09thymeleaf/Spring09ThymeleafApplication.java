f6e0a70c66c9a81478da48788752f7943a9484ab
fsanli23 <sanlifatih23@gmail.com>
2024-04-10 15:22:06 -0500

    thymeleafchanges

---
+++ Spring-08-MVC-Model/Spring-09-Thymeleaf/src/main/java/com/cydeo/spring09thymeleaf/Spring09ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring09thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09ThymeleafApplication.class, args);
+    }
+
+}
---
+++ Spring-08-MVC-Model/Spring-09-Thymeleaf/src/test/java/com/cydeo/spring09thymeleaf/Spring09ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring09thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
