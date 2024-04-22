34bd38d44d0c2a8729252a1064db5fa318246ebd
fsanli23 <sanlifatih23@gmail.com>
2024-04-21 21:19:16 -0500

    revert

--- Spring-08-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.*;
+import java.util.Random;
+import java.util.UUID;
 
 @Controller
 public class StudentController {
@@ -13,33 +14,17 @@ public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model) {
 
-        model.addAttribute("name", "fatih");
-        model.addAttribute("course", "MVC");
+        model.addAttribute("name","fatih");
+        model.addAttribute("course","MVC");
 
         UUID randomId = UUID.randomUUID();
 
-        model.addAttribute("studentId", randomId);
+        model.addAttribute("studentId",randomId);
 
-        Student student = new Student("2585258", "fatih", "sanli");
+        Student student = new Student("2585258","fatih","sanli");
 
-        model.addAttribute("student", student);
+        model.addAttribute("student",student);
 
         return "student/welcome";
     }
-
-    @RequestMapping("/register")
-    public String registerStudent(Model model) {
-
-        Student student0 = new Student("2585258", "fatih1", "sanli1");
-        Student student1 = new Student("2585258", "fatih2", "sanli2");
-        Student student2 = new Student("2585258", "fatih3", "sanli3");
-        Student student3 = new Student("2585258", "fatih4", "sanli4");
-        List<Student> list = new ArrayList<>(Arrays.asList(student0, student1, student2, student3));
-
-        model.addAttribute("myList", list);
-
-        return "student/register";
-    }
-
-
 }
--- Spring-09-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,22 +4,17 @@ import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    @RequestMapping("/register")
     public String register(Model model) {
         model.addAttribute("students", DataGenerator.createStudent());
 
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
-    public String intro() {
-        return "student/welcome";
-    }
 
 }
